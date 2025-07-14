1e3684165a2224695c26ed730bfe496d28288059
DSRGAMEING <125866681+DSRGAMEING@users.noreply.github.com>
2025-07-15 04:36:41 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -85,7 +85,7 @@ router.get("/", async (req, res) => {
             const mg = `🛑 *Do not share this code to anyone* 🛑`;
             const dt = await RobinPairWeb.sendMessage(user_jid, {
               image: {
-                url: "https://raw.githubusercontent.com/Dark-Robin/Bot-Helper/refs/heads/main/autoimage/Bot%20robin%20WP.jpg",
+                url: "https://im.ge/i/Picsart-25-07-15-04-11-39-102.J9WGZ6",
               },
               caption: sid,
             });
